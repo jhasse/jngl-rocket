8873af357238b6f7be3b20c5329777c668308c82
Jan Niklas Hasse <jhasse@bixense.com>
2018-08-28 22:48:11 +0200

    Add SystemInterface

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,12 @@
 
 #include <Rocket/Core.h>
 
+class RocketJNGLSystem : public Rocket::Core::SystemInterface {
+	float GetElapsedTime() override {
+		return jngl::getTime();
+	}
+};
+
 class RocketJNGLRenderer : public Rocket::Core::RenderInterface {
 public:
 	void RenderGeometry(Rocket::Core::Vertex* vertices, int num_vertices, int* indices,
@@ -19,18 +25,39 @@ public:
 	}
 };
 
+const int WINDOW_WIDTH = 1024;
+const int WINDOW_HEIGHT = 768;
+
 class Main : public jngl::Work {
 public:
+	Main() {
+		Rocket::Core::SetSystemInterface(&system);
+		Rocket::Core::SetRenderInterface(&renderer);
+		Rocket::Core::Initialise();
+		context = Rocket::Core::CreateContext("main",
+		                                      Rocket::Core::Vector2i(WINDOW_WIDTH, WINDOW_HEIGHT));
+		assert(context != nullptr);
+	}
+
+	~Main() {
+		context->RemoveReference();
+		Rocket::Core::Shutdown();
+	}
+
 	void step() {
 	}
 	void draw() const {
 		jngl::print("Hallo Welt!", 10, 10);
 	}
+
+private:
+	RocketJNGLSystem system;
+	RocketJNGLRenderer renderer;
+	Rocket::Core::Context* context = nullptr;
 };
 
-int main() {
-	Rocket::Core::SetRenderInterface(new RocketJNGLRenderer);
-	jngl::showWindow("JNGL+libRocket", 1920, 1080, false);
+JNGL_MAIN_BEGIN {
+	jngl::showWindow("JNGL+libRocket", WINDOW_WIDTH, WINDOW_HEIGHT, false);
 	jngl::setWork(std::make_shared<Main>());
 	jngl::mainLoop();
-}
+} JNGL_MAIN_END
