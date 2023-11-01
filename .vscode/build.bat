cfd3c1710fb3d3793fc70373630fee62ece09982
Jan Niklas Hasse <jhasse@bixense.com>
2023-11-01 18:36:07 +0100

    VSCode: Compile with MSVC on Windows

---
+++ .vscode/build.bat
@@ -0,0 +1,8 @@
+@echo off
+call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
+if exist build\build.ninja (
+    rem file exists
+) else (
+    cmake -Bbuild -G"Ninja Multi-Config" -DJNGL_VIDEO=0 -DBUILD_SHARED_LIBS=0
+)
+cmake --build build
